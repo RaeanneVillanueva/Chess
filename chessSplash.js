57a576421407dd78aad0ea9fd09b7bd46995ae73
RaeanneVillanueva <raeanne1999@gmail.com>
2019-07-31 21:31:09 +0800

    added sign up

---
+++ chessSplash.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    let gameplaydiv, signupbtn, loginbtn;
+
+    $("#playbtn").click(function(){
+        $(this).slideUp();
+        gameplaybtns();        
+    })
+
+    function gameplaybtns(){
+        gameplaydiv = document.createElement("div");
+        gameplaydiv.className = "gameplaydiv";
+        $("#borderdiv").append(gameplaydiv);
+
+        singlebtn = document.createElement("a");
+        singlebtn.className = "gameplaybtn animated fadeInUp hvr-sweep-to-right";
+        $(singlebtn).text("Sign Up!");
+        $(singlebtn).css("cursor", "pointer");
+        gameplaydiv.appendChild(signupbtn);
+
+        multibtn = document.createElement("a");
+        multibtn.className = "gameplaybtn animated fadeInUp hvr-sweep-to-right";
+        $(multibtn).text("Log In");
+        $(multibtn).css("cursor", "pointer");
+        gameplaydiv.appendChild(loginbtn);
+    }
+
+})
--- index.js
+++ index.js
@@ -1,1 +1,49 @@
+var express = require("express")
+const bodyparser = require("body-parser")
+const app = express()
+const session = require("express-session")
+const urlencoder = bodyparser.urlencoded({
+    extended: false
+})
+const cookieparser = require("cookie-parser")
+const mongoose = require("mongoose")
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost:27017/users", {
+    useNewUrlParser: true
+})
+
+const {User} = require("./user.js")
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/public/chessSplash.html");
+})
+
+app.post("/signup", urlencoder, function(req, res){
+    var username = req.body.un
+    var password = req.body.pw
+
+    let user = new User({
+        username,
+        password
+    })
+
+    user.save().then(function(doc){
+        console.log(doc);
+        res.render("chessHome.hbs", {
+            username: doc.username
+        })
+    },function(err){
+        res.send(err);
+    })
+})
+
+
+
+
+
+app.listen(3000, function(req,res){
+    console.log("port is listening...");
+})
 
