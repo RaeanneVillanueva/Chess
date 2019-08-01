4df7f211c0758ba89710c6832bd9ac3d6d9ee793
John Andrew Ing <john_andrew_ing@dlsu.edu.ph>
2019-08-01 19:11:08 +0800

    update

--- index.js
+++ index.js
@@ -12,15 +12,31 @@ mongoose.connect("mongodb://localhost:27017/users", {
     useNewUrlParser: true
 })
 
-const {User} = require("./user.js")
+const { User } = require("./user.js")
 
 app.use(express.static(__dirname + "/public"));
+app.use(session({
+    secret: "LOGIN_INFO",
+    name: "login",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 2
+    }
+}))
+app.use(cookieparser())
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/public/chessSplash.html");
+app.get("/", function (req, res) {
+    console.log(req.session.username)
+    if (!req.session.username)
+        res.sendFile(__dirname + "/public/splash.html")
+    else
+        res.render("home.hbs", {
+            username: req.session.username
+        })
 })
 
-app.post("/signup", urlencoder, function(req, res){
+app.post("/signup", urlencoder, function (req, res) {
     var username = req.body.un
     var password = req.body.pw
 
@@ -30,41 +46,43 @@ app.post("/signup", urlencoder, function(req, res){
         elo: 1200
     })
 
-    user.save().then(function(doc){
+    user.save().then(function (doc) {
         console.log(doc);
-        res.render("chessHome.hbs", {
-            username: doc.username
+        req.session.username = doc.username
+
+        res.render("home.hbs", {
+            username: doc.username,
+            elo: doc.elo
         })
-    },function(err){
+    }, function (err) {
         res.send(err);
     })
 })
 
 
-app.post("/login", urlencoder,function(req,res){
+app.post("/login", urlencoder, function (req, res) {
     let username = req.body.un
     let password = req.body.pw
     User.findOne({
         username,
         password
-    }, function(err, doc){
-        if(err){
+    }, function (err, doc) {
+        if (err) {
             res.send(err)
-        }else if(doc){
-            console.log(doc)
-            res.render("chessHome.hbs",{
+        } else if (doc) {
+            req.session.username = doc.username
+            res.render("home.hbs", {
                 username: doc.username,
                 elo: doc.elo
             })
-        }else {
+        } else {
             res.send("User not found!")
         }
     })
 })
 
 
-app.listen(3000, function(req,res){
+app.listen(3000, function (req, res) {
     console.log("port 3000 is listening...");
 })
 
- 
--- user.js
+++ user.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose")
 var User = mongoose.model("user",{
     username: String,
     password: String,
-    elo: Number
+    elo: Number,
+    wins: Number,
+    loses: Number
 })
 
 module.exports = {
