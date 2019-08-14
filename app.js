16e2a2d65a1f58e0f1be41e9c78e88bc89b7c8c9
John Andrew Ing <john_andrew_ing@dlsu.edu.ph>
2019-08-14 15:54:45 +0800

    fixed the login

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect("mongodb://localhost:27017/chessusers", {
 })
 app.set("view engine", "hbs")
 app.use(express.static(__dirname + "/public"));
-// const { User } = require("./model/user.js/index.js")
+
 
 app.use(session({
     secret: "LOGIN_INFO",
@@ -29,64 +29,6 @@ app.use(cookieparser())
 app.use(require("./controllers"))
 
 
-
-
-
-
-// app.post("/login", urlencoder, function (req, res) {
-//     let username = req.body.un
-//     let password = req.body.pw
-//     User.findOne({
-//         username,
-//         password
-//     }, function (err, doc) {
-//         if (err) {
-//             res.send(err)
-//         } else if (doc) {
-//             req.session.username = doc.username
-//             res.redirect("/")
-//         } else {
-//             res.send("User not found!")
-//         }
-//     })
-// })
-
-// app.get("/login", function(req,res){
-//     res.sendFile(__dirname + "/public/login.html")
-// })
-
-// app.get("/signup", function(req,res){
-//     res.sendFile(__dirname+ "/public/signup.html")
-// })
-
-// app.get("/play", function(req,res){
-
-//     if(req.session.username){
-//         res.render("play.hbs", {
-//             username: req.session.username
-//         })
-//     }else{
-//         res.redirect("/")
-//     }
-// })
-
-// app.get("/profile", function(req,res){
-//     if(req.session.username){
-//         res.render("profile.hbs", {
-//             username: req.session.username,
-//             elo: req.session.elo
-//         })
-//     }else{
-//         res.redirect("/")
-//     }
-// })
-
-// app.get("/logout", function(req,res){
-//     req.session.destroy((err)=>{
-//         console.log("ERROR destroying cookie")
-//     })
-//     res.redirect("/")
-// })
 
 app.listen(3000, function (req, res) {
     console.log("port 3000 is listening...");
--- controllers/index.js
+++ controllers/index.js
@@ -8,15 +8,14 @@ router.use("/rooms", require("./rooms"))
 
 router.get("/", function (req, res) {
     console.log("GET /")
-    if (!req.session.username)
-        res.sendFile("index.html")
-    else
+    if (!req.session.username){
+        res.render("index",{})
+    }else{
         res.render("home", {
             username: req.session.username
         })
+    }
 })
-
-
 
 router.get("/profile", function(req,res){
     console.log("GET /profile")
@@ -38,7 +37,7 @@ router.get("/logout", function(req,res){
 })
 
 router.get("*", function(req,res){
-    res.send("Queit out here (・_・) . . . Oh, we didn't find the page you were looking for . . . sorry")
+    res.send("Quiet out here (・_・) . . . Oh, we didn't find the page you were looking for . . . sorry")
 })
 
 module.exports = router
