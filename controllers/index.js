718954d7805e85287e5a967920b71e7af18b7b63
RaeanneVillanueva <raeanne1999@gmail.com>
2019-08-22 20:52:14 +0800

    added about us

--- controllers/index.js
+++ controllers/index.js
@@ -10,7 +10,7 @@ router.get("/", function (req, res) {
     if (!req.session.username){
         res.render("index.hbs")
     }else{
-        res.render("home", {
+        res.render("aboutUs", {
             username: req.session.username
         })
     }
